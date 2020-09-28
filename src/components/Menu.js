9e56aaa862363d846b1d27f0914c568b16a9fc4c
kamorales <kamoralescarranza@gmail.com>
2020-09-28 16:08:57 -0500

    First Commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,52 +6,26 @@ import { data } from "../resources/numberCategories";
 const Menu = ({ defaultUser, onGet }) => {
   onGet(data[defaultUser]);
   return (
-    <div className = "Header">
+    <div className="Header">
       <div className="Search">
-        <h1>News Feed </h1>
-        <div>
-          <input type="text" placeholder="Search" />
-          <i className=" fa fa-search fa-lg"></i>
-        </div>
+        <h1>Electronics </h1>
       </div>
       <div className="Categorias">
         <nav>
           <ul>
             <li>
               <Link to={"/home"}>
-                <i className="fa fa-home fa-lg"></i> Home
+                Most recent
               </Link>
             </li>
             <li>
               <Link to={"/politics"}>
-                <i className="fa fa-gavel fa-lg"></i> Política
+                Lowest price
               </Link>
             </li>
             <li>
               <Link to={"/international"}>
-                <i className="fa fa-globe fa-lg "></i> Internacionales
-              </Link>
-            </li>
-            <li>
-              <Link to={"/tech"}>
-                <i className="fa fa-laptop fa-lg "></i> Tecnología
-              </Link>
-            </li>
-            <li>
-              <Link to={"/shows"}>
-                <i className="fa fa-film fa-lg "></i> Espectáculos
-              </Link>
-            </li>
-
-            <li>
-              <Link to={"/sports"}>
-                <i className="fa fa-futbol-o fa-lg "></i> Deportes
-              </Link>
-            </li>
-
-            <li>
-              <Link to={"/design"}>
-                <i className="fa fa-paint-brush fa-lg "></i> Diseño
+                Highest price
               </Link>
             </li>
           </ul>
